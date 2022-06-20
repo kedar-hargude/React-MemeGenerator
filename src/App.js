b3e0a1de71315b360aba5ded0e8313d62ddc98a6
Kedar Hargude <hargude481@gmail.com>
2022-06-21 00:37:28 +0530

    scrimba first half module

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Header } from "./components/Header"
+import { Main } from "./components/Main"
+
+export function App(){
+    return(
+        <div>
+            <Header />
+            <Main />
+        </div>
+    )
+}
---
+++ src/components/Main.js
@@ -0,0 +1,46 @@
+import React from "react"
+import memesData from "../memesData"
+
+export function Main(){
+
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
+    })
+
+    const [allMemeImages, setAllMemeImages] = React.useState(memesData)
+
+    function getMemeImage(){
+        const allMemesArray = allMemeImages.data.memes;
+        const randomNumber = Math.floor(Math.random() * allMemesArray.length);
+        const url = allMemesArray[randomNumber].url;
+        setMeme(prevState => {
+            return{...prevState, randomImage: url}
+        })
+    }
+
+    return(
+        <main>
+            <div className="form">
+                <input 
+                    type="text"
+                    placeholder="Top text"
+                    className="form--input"
+                />
+                <input 
+                    type="text"
+                    placeholder="Bottom text"
+                    className="form--input"
+                />
+                <button 
+                    className="form--button"
+                    onClick={getMemeImage}
+                >
+                    Get a new meme image 🖼
+                </button>
+            </div>
+            <img src={meme.randomImage} className="meme--image" />
+        </main>
+    )
+}
